20885b8d0a5052aa3680a292dc243fa4b8838b95
Gvozdev Nikita <gvozdev.public@gmail.com>
2022-05-09 16:30:44 +0300

    Connect buttons with bot

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
 import telebot
+from telebot import types
+
+import buttons
+import dbworker
+import config
+
 import json
 import os
-from pprint import pprint 
 
-def get_token():
-    f = open("token", "r")
-    token = f.read()
-    f.close()
-    return token
+bot = telebot.TeleBot(config.token)
 
-bot = telebot.TeleBot(get_token())
 sessions = []
+PAGE = 0
+ITEMS_ON_PAGE = 3
 
 def get_game(game_name):
     with open("games/"+game_name+".json", 'r', encoding='utf-8') as f:
@@ -20,62 +22,201 @@ def get_game(game_name):
 def add_new_game(user_id, game):
     global sessions
     if sessions == []:
-        sessions = [{"user_id": user_id, "game_name": game["Name"], "possible texts": game["Available texts"]}]
+        sessions = [{"user_id": user_id, "game_name": game["Name"], "possible_texts": game["Available texts"], "current_text": 0}]
     else:
-        sessions.append({"user_id": user_id, "game_name": game["Name"], "possible texts": game["Available texts"]})
+        sessions.append({"user_id": user_id, "game_name": game["Name"], "possible_texts": game["Available texts"], "current_text": 0})
+
+def update_current_text(user_id, text_id):
+    global sessions
+    session_id = 0
+    for i, session in enumerate(sessions):
+        if session["user_id"] == user_id:
+            session_id = i
+    
+    sessions[session_id]["current_text"] = text_id
+
+def update_possible_texts(user_id, text_id):
+    global sessions
+    session_id = 0
+    for i, session in enumerate(sessions):
+        if session["user_id"] == user_id:
+            session_id = i
+    
+    sessions[session_id]["possible_texts"].append(text_id)
 
 def exit_game(user_id):
     global sessions
     for i, session in enumerate(sessions):
         if session["user_id"] == user_id:
-            sessions.erase(i)
+            sessions.pop(i)
             return
 
-def start_game(message):
-    user_id = message.chat.id
-    game_name = message.text
+def start_game(user_id, game_name):
     game = get_game(game_name)
 
-    add_new_game(user_id, game)
-
-    bot.send_message(message.chat.id, game["Name"])
-    bot.send_message(message.chat.id, game["Start text"])
+    bot.send_message(user_id, game["Name"])
+    bot.send_message(user_id, game["Start text"])
+    markup_array = [1] * len(game["Available texts"])
+    string_array = [game["Texts"][i]["Name"] for i in game["Available texts"]]
+    keyboard = buttons.button_generator(markup_array, string_array)
     for text_id in game["Available texts"]:
-        bot.send_message(message.chat.id, game["Texts"][text_id - 1]["Bot text"])
+        bot.send_message(user_id, game["Texts"][text_id]["Name"], reply_markup = keyboard)
+
+
+def choose_game(message):
+    global ITEMS_ON_PAGE
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True, one_time_keyboard = True)
+    dirname = os.getcwd() + "/games"
+    filenames = os.listdir(dirname)
+    counter = 0
+    items = ITEMS_ON_PAGE
+    if len(filenames) < ITEMS_ON_PAGE:
+        items = len(filenames)
+    for i in range (items):
+        keyboard.add(filenames[i][:-5])
+    if len(filenames) > ITEMS_ON_PAGE:
+        keyboard.add("Далее")
+    bot.send_message(message.chat.id, "Выберите игру:", reply_markup = keyboard)
+    dbworker.set_state(message.chat.id, config.States.S_CHOOSE_GAME.value)
+
+def all_texts_keyboard(game, possible_texts):
+    markup_array = [1] * (len(possible_texts) + 1)
+    string_array = [game["Texts"][i]["Name"] for i in possible_texts]
+    string_array.append("Начальный текст")
+    keyboard = buttons.button_generator(markup_array, string_array)
+    return keyboard
+
+def all_texts_string(game, possible_texts):
+    bot_str = "Доступные тексты:\n\n"
+    for text_id in possible_texts:
+        bot_str += "\n" + game["Texts"][text_id]["Name"]
+    bot_str += "\nНачальный текст"
+    return bot_str
+
+def answers_keyboard(answers):
+    markup_array = [1] * len(answers)
+    markup_array.append(1)
+    string_array = answers
+    string_array.append("Все тексты")
+    keyboard = buttons.button_generator(markup_array, string_array)
+    return keyboard
+
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_CHOOSE_GAME.value)
+def list_of_games_paging(message):
+    global PAGE
+    global ITEMS_ON_PAGE
+    if message.text == "Далее":
+        PAGE = PAGE + 1
+    elif message.text == "Назад":
+        PAGE = PAGE - 1
+    else:
+        dbworker.set_state(message.chat.id, config.States.S_CHOOSE_TEXT.value)
+        add_new_game(message.chat.id, get_game(message.text))
+        game_name = ""
+        for session in sessions:
+            if session["user_id"] == message.chat.id:
+                game_name = session["game_name"]
+        start_game(message.chat.id, game_name)
+        return
+    
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True, one_time_keyboard = True)
+    dirname = os.getcwd() + "/games"
+    filenames = os.listdir(dirname)
+    items = PAGE * ITEMS_ON_PAGE + ITEMS_ON_PAGE
+    if len(filenames) < items:
+        items = len(filenames)
+    for i in range (PAGE * ITEMS_ON_PAGE, items):
+        keyboard.add(filenames[i][:-5])
+    if PAGE == 0:
+        keyboard.add("Далее")
+    else:
+        if len(filenames) > PAGE * ITEMS_ON_PAGE + ITEMS_ON_PAGE:
+            keyboard.add("Назад", "Далее")
+        else:
+            keyboard.add("Назад")
+    msg = bot.send_message(message.chat.id, "Выберите игру:", reply_markup = keyboard)
 
 @bot.message_handler(commands=["start"])
 def start(message, res=False):
-    dirname = os.getcwd() + "/games"
-    files = os.listdir(dirname)
+    user_id = message.chat.id
+    choose_game(message)
 
-    bot.send_message(message.chat.id, "Приветствую! Выбери игру:")
-    for game in files:
-        bot.send_message(message.chat.id, game[:-5])
-
-    bot.register_next_step_handler(message, start_game)
-
-
-@bot.message_handler(content_types=["text"])
-def state(message):
+@bot.message_handler(content_types=["text"], func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_CHOOSE_TEXT.value)
+def step(message):
     global sessions
     user_id = message.chat.id
     game_name = ""
+    possible_texts = []
+    for i, session in enumerate(sessions):
+        if session["user_id"] == user_id:
+            game_name = session["game_name"]
+            possible_texts = session["possible_texts"]
+    game = get_game(game_name)
+
+    if message.text == "Начальный текст":
+        markup_array = [1]
+        string_array = ["Все тексты"]
+        keyboard = buttons.button_generator(markup_array, string_array)
+        bot.send_message(user_id, game["Start text"], reply_markup = keyboard)
+
+    if message.text == "Все тексты":
+        keyboard = all_texts_keyboard(game, possible_texts)
+        bot_str = all_texts_string(game, possible_texts)
+        bot.send_message(user_id, bot_str, reply_markup = keyboard)
+
+    if message.text != "Начальный текст" and message.text != "Все тексты":
+        text_id = 0
+        for text in game["Texts"]:
+            if text["Name"] == message.text:
+                text_id = text["Id"]
+        update_current_text(user_id, text_id)
+        keyboard = answers_keyboard(game["Texts"][text_id]["Answers"])
+        bot.send_message(user_id, game["Texts"][text_id]["Bot text"], reply_markup = keyboard)
+        dbworker.set_state(message.chat.id, config.States.S_CHOOSE_ANSWER.value)
+
+@bot.message_handler(content_types=["text"], func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_CHOOSE_ANSWER.value)
+def choose_answer(message):
+    global sessions
+    user_id = message.chat.id
+    game_name = ""
+    text_id = 0
+    possible_texts = []
     for session in sessions:
         if session["user_id"] == user_id:
             game_name = session["game_name"]
-
+            text_id = session["current_text"]
+            possible_texts = session["possible_texts"]
     game = get_game(game_name)
-    state_id = int(message.text)
 
-    if state_id == 0:
-        bot.send_message(message.chat.id, game["Name"])
-        bot.send_message(message.chat.id, game["Start text"])
+    if message.text == "Все тексты":
+        dbworker.set_state(message.chat.id, config.States.S_CHOOSE_TEXT.value)
+        keyboard = all_texts_keyboard(game, possible_texts)
+        bot_str = all_texts_string(game, possible_texts)
+        bot.send_message(user_id, bot_str, reply_markup = keyboard)
+        return
 
-    if state_id == -1:
-        bot.send_message(message.chat.id, game["Final text"])
-        exit_game(message.chat.id)
+    answer_id = 0
+    for i, answer in enumerate(game["Texts"][text_id]["Answers"]):
+        if answer == message.text:
+            answer_id = i
+    
+    if answer_id == game["Texts"][text_id]["Right answer"]:
+        if game["Texts"][text_id]["Next text"] == -1:
+            dbworker.set_state(message.chat.id, config.States.S_CHOOSE_GAME.value)
+            bot.send_message(user_id, game["Final text"])
+            exit_game(user_id)
+            choose_game(message)
+        else:
+            update_possible_texts(user_id, game["Texts"][text_id]["Next text"])
+            dbworker.set_state(message.chat.id, config.States.S_CHOOSE_TEXT.value)
+            keyboard = all_texts_keyboard(game, possible_texts)
+            bot_str = all_texts_string(game, possible_texts)
+            bot.send_message(user_id, bot_str, reply_markup = keyboard)
+    else:
+        bot.send_message(user_id, "К сожалению, это не верный ответ.")
+        keyboard = answers_keyboard(game["Texts"][text_id]["Answers"])
+        bot.send_message(user_id, game["Texts"][text_id]["Bot text"], reply_markup = keyboard)
 
-    if state_id != 0 and state_id != 1:
-            bot.send_message(message.chat.id, game["Texts"][state_id]["Bot text"])
+
 
 bot.polling(none_stop=True, interval=0)
--- buttons.py
+++ buttons.py
@@ -1,17 +1,4 @@
-import telebot
 from telebot import types
-import os
-
-PAGE = 0
-ITEMS_ON_PAGE = 3
-
-def get_token():
-    f = open("token", "r")
-    token = f.read()
-    f.close()
-    return token
-
-bot = telebot.TeleBot(get_token())
 
 def button_generator(markup_array, string_array):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True, one_time_keyboard = True)
@@ -25,53 +12,3 @@ def button_generator(markup_array, string_array):
 
     return keyboard
     #returning ReplyKeyboardMarkup
-
-@bot.message_handler(commands="list")
-def list_of_games(message):
-    global ITEMS_ON_PAGE
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True, one_time_keyboard = True)
-    dirname = os.getcwd() + "/games"
-    filenames = os.listdir(dirname)
-    print(filenames)
-    counter = 0
-    items = ITEMS_ON_PAGE
-    if len(filenames) < ITEMS_ON_PAGE:
-        items = len(filenames)
-    for i in range (items):
-        keyboard.add(filenames[i])
-    if len(filenames) > ITEMS_ON_PAGE:
-        keyboard.add("Далее")
-    msg = bot.send_message(message.chat.id, "Выберите игру:", reply_markup = keyboard)
-    game_name = bot.register_next_step_handler(msg, list_of_games_paging)
-    return game_name
-
-def list_of_games_paging(message):
-    global PAGE
-    global ITEMS_ON_PAGE
-    if message.text == "Далее":
-        PAGE = PAGE + 1
-    elif message.text == "Назад":
-        PAGE = PAGE - 1
-    else:
-        return message.text
-    
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True, one_time_keyboard = True)
-    dirname = os.getcwd() + "/games"
-    filenames = os.listdir(dirname)
-    items = PAGE * ITEMS_ON_PAGE + ITEMS_ON_PAGE
-    if len(filenames) < items:
-        items = len(filenames)
-    for i in range (PAGE * ITEMS_ON_PAGE, items):
-        keyboard.add(filenames[i])
-    if PAGE == 0:
-        keyboard.add("Далее")
-    else:
-        if len(filenames) > PAGE * ITEMS_ON_PAGE + ITEMS_ON_PAGE:
-            keyboard.add("Назад", "Далее")
-        else:
-            keyboard.add("Назад")
-    msg = bot.send_message(message.chat.id, "Выберите игру:", reply_markup = keyboard)
-    bot.register_next_step_handler(msg, list_of_games_paging)
-
-
-bot.polling(non_stop = True)
