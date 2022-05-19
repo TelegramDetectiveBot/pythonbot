72c3579c56397db927129d6a52f5d2037d749a41
Mikhail Chernov <mixailchernov8@gmail.com>
2022-05-20 00:54:09 +0300

    get rid of vedis

--- dbworker.py
+++ dbworker.py
@@ -1,20 +1,30 @@
-from vedis import Vedis
 import config
+import csv
 
 # Пытаемся узнать из базы «состояние» пользователя
 def get_current_state(user_id):
-    with Vedis(config.db_file) as db:
+    dict_from_csv = {}
+
+    with open(config.db_file, mode='r') as inp:
+        reader = csv.reader(inp)
+        dict_from_csv = {rows[0]:rows[1] for rows in reader}
         try:
-            return db[user_id].decode() # Если используете Vedis версии ниже, чем 0.7.1, то .decode() НЕ НУЖЕН
-        except KeyError:  # Если такого ключа почему-то не оказалось
-            return config.States.S_START.value  # значение по умолчанию - начало диалога
+            return dict_from_csv[str(user_id)]
+        except:
+            return config.States.S_START.value
 
 # Сохраняем текущее «состояние» пользователя в нашу базу
 def set_state(user_id, value):
-    with Vedis(config.db_file) as db:
+    dict_from_csv = {}
+    with open(config.db_file, mode='r') as inp:
+        reader = csv.reader(inp)
+
+        dict_from_csv = {rows[0]:rows[1] for rows in reader}
         try:
-            db[user_id] = value
-            return True
+            dict_from_csv[str(user_id)] = value
         except:
             # тут желательно как-то обработать ситуацию
             return False
+    with open(config.db_file, 'w') as f:
+        for key in dict_from_csv.keys():
+            f.write("%s,%s\n" % (key, dict_from_csv[key]))
